063c44ea9cd3292036c78e041bf41bbf284b53e5
kaedenagomiya <nagomiya75328@gmail.com>
2025-03-06 10:25:21 +0900

    [modify] Add parameters for graph drawing and paths for data to be analyzed.

--- umakemosfig.py
+++ umakemosfig.py
@@ -7,20 +7,14 @@ import seaborn as sns
 
 #model_names_list = ['ljspeech',  'gradtts', 'gradseptts', 'gradtfktts', 'gradtfk5tts', 'nix_deter']
 model_names_list = ['ljspeech',  'gradtts', 'gradtfktts', 'gradtfk5tts', 'nix_deter']
-DATA_DIR = Path('./results_evaluation')
-SAVE_DIR = Path('./results_fig_mos')
-#subject_id = "2b005cae-245a-4d53-89fa-b02c3cce53ad"
-#subject_id = "924af422-dc2c-4880-8c39-e512c83e8448"
-
-#subject_id = "5f558e4e-d68f-4a94-aaf8-ebf6df7460d9"
-#subject_id = "0136e521-d845-496d-b649-e59deb3c5a07"
-#subject_id = "d14b1fd9-5fb5-4bef-bfef-fa02e24c0d4e"
-#subject_id = "df1d30e3-207c-45c3-9bc3-10bad7de63b7"
-# [tmp] = "588291ca-85cc-4301-842c-015334541856"
-#subject_id = "81a7080b-7d46-42f8-b97a-914110ace219" #[x]
+rename_list = ['ljspeech', 'gradtts', 'gradtfktts', 'gradtfk5tts', 'nix deter']
+color_list = ['#ff6700', '#FF0000', '#85ACFF', '#245DFE', '#468585']
+hatch_list = ['','','','','']
 y_min = 1
 y_max = 5.4
-color_list = ['#ff6700', '#FF0000', '#85ACFF', '#245DFE', '#468585']
+
+DATA_DIR = Path('./results_evaluation')
+SAVE_DIR = Path('./results_fig_mos')
 
 for subject_dir in DATA_DIR.iterdir():
     if not subject_dir.is_dir():
@@ -43,6 +37,10 @@ for subject_dir in DATA_DIR.iterdir():
     mos_means = mos_means.reindex(model_names_list)
     mos_std = df_eval.groupby("model_name")["evaluation"].std()
     mos_std = mos_std.reindex(model_names_list) 
+
+    print(mos_means)
+    print(type(mos_means))
+    print(mos_means.shape)
 
     # for figure
     fig, ax = plt.subplots(figsize=(8, 5))
